aa2a32df0fad6d3d0928c8db7b43625e90b58623
willystout <123514204+willystout@users.noreply.github.com>
2024-12-05 00:21:31 -0800

    fixed footer spacing and api key privacy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,21 +8,31 @@ function Footer() {
     return (
         <footer className={styles['footer-container']}>
             <div className={styles['footer-content']}>
-                <Image
-                    src="/usflogo.png"
-                    alt="USF Logo"
-                    width={100}
-                    height={100}
-                    className="w-auto h-10"
-                    priority
-                />
-                <p>&copy; 2024 GeoAI. All rights reserved.</p>
-                <nav>
-                    <ul className={styles['footer-links']}>
-                        <li><a href="/contact">Contact</a></li>
-                        <li><a href="https://github.com/willystout/geo_ai_app">GitHub</a></li>
-                    </ul>
-                </nav>
+                {/* logo */}
+                <div className={styles['footer-logo']}>
+                    <Image
+                        src="/usflogo.png"
+                        alt="USF Logo"
+                        width={100}
+                        height={100}
+                        className={styles['logo-image']}
+                        priority
+                    />
+                </div>
+                
+                {/* Center content */}
+                <div className={styles['footer-center']}>
+                    <p className={styles['copyright']}>© 2024 GeoAI. All rights reserved.</p>
+                    <nav>
+                        <ul className={styles['footer-links']}>
+                            <li><a href="/contact">Contact</a></li>
+                            <li><a href="https://github.com/willystout/geo_ai_app">GitHub</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                
+                {/* Empty div to keep logo on the left*/}
+                <div className={styles['footer-right']} />
             </div>
         </footer>
     );
